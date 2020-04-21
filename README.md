6b2b65f6605bedd99acccbd0e27f24eb09f5616a
Jake Cyr <cyrjake@gmail.com>
2020-04-21 19:15:57 -0400

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Finance Calculator
+
+Simple financial calculation functions
+
+## Methods include
+
+* Present to future value
+* Future to present value
+* Insert rate from present / future values
+* RSI with given values / length
+* SMA (simple moving average) with given values / length
---
+++ main.js
@@ -0,0 +1,55 @@
+module.exports = {
+    presentToFutureValue: (presentValue, interestRate, periods) => {
+        return presentValue * ((1 + interestRate) ** periods);
+    },
+    futureToPresentValue: (futureValue, interestRate, periods) => {
+        return futureValue / ((1 + interestRate) ** periods);
+    },
+    interestRateFromPresentFuture: (presentValue, futureValue, periods) => {
+        return ((futureValue / presentValue) ** (1 / periods)) - 1;
+    },
+    calculateSMA: (valuesArr, length) => {
+        const values = valuesArr.slice(-length);
+
+        if (values.length > 0) {
+            return values.reduce((a, b) => (parseInt(a) || 0) + (parseInt(b) || 0)) / values.length;
+        } else {
+            return null;
+        }
+    },
+    calculateRSI: (valuesArr, length) => {
+        let ups = [];
+        let downs = [];
+
+        const lastPrices = valuesArr.slice(-(length + 1));
+
+        for (let i = 1; i < lastPrices.length - 1; i++) {
+            const currentPrice = lastPrices[i];
+            const lastPrice = lastPrices[i - 1];
+            const diff = currentPrice - lastPrice;
+
+            if (diff > 0) {
+                ups.push(diff);
+                downs.push(0);
+            } else if (diff < 0) {
+                downs.push(Math.abs(diff));
+                ups.push(0);
+            } else {
+                downs.push(0);
+                ups.push(0);
+            }
+        }
+
+        if (ups.length > 0 && downs.length > 0) {
+            const averageUp = ups.reduce((a, b) => a + b) / ups.length;
+            const averageDown = downs.reduce((a, b) => a + b) / downs.length;
+
+            const rs = averageUp / averageDown;
+            const rsi = 100 - (100 / (1 + rs));
+
+            return rsi;
+        } else {
+            return null;
+        }
+    },
+};
